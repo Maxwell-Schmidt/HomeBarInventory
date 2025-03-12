8ee5e5fe5d480154fa1467b79301981c7561bf13
Maxwell-Schmidt <max.r.schmidt@icloud.com>
2025-03-11 21:35:18 -0500

    ÃInitial setup

---
+++ HomeBarInventory/src/main/java/api/HomeBarInventory/HomeBarInventoryApplication.java
@@ -0,0 +1,13 @@
+package api.HomeBarInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBarInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeBarInventoryApplication.class, args);
+	}
+
+}
